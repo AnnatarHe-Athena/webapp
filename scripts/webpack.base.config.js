9f722823c004dddb04bfc2b01c7959ef75897030
AnnatarHe <iamhele1994@gmail.com>
2017-10-01 19:09:21 +0800

    update for server render

--- scripts/webpack.base.config.js
+++ scripts/webpack.base.config.js
@@ -6,107 +6,107 @@ const AddAssertHtmlPlugin = require('add-asset-html-webpack-plugin')
 const poststylus = require('poststylus')
 
 const config = {
-    entry: [
-        'babel-polyfill',
-        'whatwg-fetch',
-        'react-hot-loader/patch',
-        './src/index.js'
-    ],
-    output: {
-        path: path.resolve(__dirname, '..', 'dist'),
-        publicPath: '/',
-        filename: 'bundle.[hash].js'
-    },
-    module: {
-        rules: [{
-            test: /.jsx?$/,
-            exclude: [path.resolve(__dirname, '..', 'node_modules')],
-            use: ['babel-loader']
-        }, {
-            test: /.styl$/,
-            exclude: /node_modules/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [ 'css-loader', 'stylus-loader' ]
-            })
-        }, {
-            test: /.css$/,
-            exclude: /node_modules/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [
-                    'css-loader?modules=true&camelCase=true&localIdentName=[name]_[local]-[hash:base64]&sourceMap=true',
-                    'postcss-loader'
-                ]
-            })
-        }, {
-            test: /.css$/,
-            include: /node_modules/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader']
-            })
-        }, {
-            test: /\.(png|jpg|jpeg|gif)$/,
-            use: [{loader: 'url-loader', options: {limit: 500, name: '[name]-[hash].[ext]'}}]
-        }, {
-            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'application/font-woff'}}]
-        }, {
-            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'application/font-woff'}}]
-        }, {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'application/octet-stream'}}]
-        }, {
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{loader: 'file-loader'}]
-        }, {
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'image/svg+xml'}}]
-        }, {
-            test: /\.(graphql|gql)$/,
-            exclude: /node_modules/,
-            // include: [path.resolve('..', '..', 'schema')],
-            loader: 'graphql-tag/loader',
-      }]
+  entry: [
+    'babel-polyfill',
+    'whatwg-fetch',
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, '..', 'dist'),
+    publicPath: '/',
+    filename: 'bundle.[hash].js'
   },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Athena webapp',
-            inject: 'body',
-            template: path.resolve(__dirname, 'template.html')
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common',
-            filename: '[name].[hash:8].js',
-            minChunks: Infinity
-        }),
-        new ExtractTextPlugin({
-            filename: 'app.[contenthash:8].css',
-            disable: false,
-            allChunks: true
-        }),
-        new AddAssertHtmlPlugin({
-          filepath: path.resolve(__dirname, '..', 'dist', '*.dll.js'),
-          includeSourcemap: process.env.NODE_ENV !== 'production'
-        }),
-        new webpack.LoaderOptionsPlugin({
-            debug: true,
-            stylus: {
-                default: {
-                    use: [poststylus([require('autoprefixer')])]
-                }
-            }
-        })
-    ],
-    resolve: {
-      extensions: ['.js', '.jsx', '.styl'],
-      alias: {
-        AthenaSchema: path.resolve(__dirname, '..', '..', 'schema'),
-        AthenaComponents: path.resolve(__dirname, '..', 'src', 'components')
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      exclude: [path.resolve(__dirname, '..', 'node_modules')],
+      use: ['babel-loader']
+    }, {
+      test: /.styl$/,
+      exclude: /node_modules/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [ 'css-loader', 'stylus-loader' ]
+      })
+    }, {
+      test: /.css$/,
+      exclude: /node_modules/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader?modules=true&camelCase=true&localIdentName=[name]_[local]-[hash:base64]&sourceMap=true',
+          'postcss-loader'
+        ]
+      })
+    }, {
+      test: /.css$/,
+      include: /node_modules/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader']
+      })
+    }, {
+      test: /\.(png|jpg|jpeg|gif)$/,
+      use: [{loader: 'url-loader', options: {limit: 500, name: '[name]-[hash].[ext]'}}]
+    }, {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'application/font-woff'}}]
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'application/font-woff'}}]
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'application/octet-stream'}}]
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{loader: 'file-loader'}]
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{loader: 'url-loader', options: {limit: 10000, mimetype: 'image/svg+xml'}}]
+    }, {
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      // include: [path.resolve('..', '..', 'schema')],
+      loader: 'graphql-tag/loader',
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Athena webapp',
+      inject: 'body',
+      template: path.resolve(__dirname, 'template.html')
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: '[name].[hash:8].js',
+      minChunks: Infinity
+    }),
+    new ExtractTextPlugin({
+      filename: 'app.[contenthash:8].css',
+      disable: false,
+      allChunks: true
+    }),
+    new AddAssertHtmlPlugin({
+      filepath: path.resolve(__dirname, '..', 'dist', '*.dll.js'),
+      includeSourcemap: process.env.NODE_ENV !== 'production'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+      stylus: {
+        default: {
+          use: [poststylus([require('autoprefixer')])]
+        }
       }
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.styl'],
+    alias: {
+      AthenaSchema: path.resolve(__dirname, '..', '..', 'schema'),
+      AthenaComponents: path.resolve(__dirname, '..', 'src', 'components')
     }
+  }
 }
 
 const dllRefs = ['utils', 'vendors', 'plugins']
