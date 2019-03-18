500aa950bfbb093ac17e75a719d4924a21877dce
AnnatarHe <iamhele1994@gmail.com>
2019-03-18 22:22:50 +0800

    perf(html): speed up html load and add fathom track

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'normalize.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
