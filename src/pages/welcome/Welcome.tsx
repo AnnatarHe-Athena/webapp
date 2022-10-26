9c22f465e08eb6fee3e08a2355918451cc8b396d
AnnatarHe <annatar.he+github@gmail.com>
2022-10-27 00:58:19 +0800

    fix(welcome): update welcome page

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Welcome() {
   return (
     <section className='flex justify-center items-center w-full'>
-      <h2 className='mt-8 dark:text-white'> welcome </h2>
+      <h2 className='mt-32 text-center dark:text-white'>Welcome</h2>
     </section>
   )
 }
