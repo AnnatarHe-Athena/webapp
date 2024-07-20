f6915ae07608247efedaf9dfa2afc36250aa4c27
AnnatarHe <annatar.he+github@gmail.com>
2024-07-20 12:05:03 +0800

    fix(css): fix tailwindcss config in monorepo

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   content: [
-    "./src/**/*.ts",
-    "./src/**/*.tsx",
-    "./src/**/*.jsx",
-    "./src/**/*.js",
+    "../components/**/*.{ts,tsx,js,jsx}",
+    "./src/**/*.{ts,tsx,js,jsx}",
   ],
   // darkMode: false, // or 'media' or 'class'
   theme: {
