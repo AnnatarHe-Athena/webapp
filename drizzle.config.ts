b476a5dff4de9304a15bb6caf80f9303f12639aa
AnnatarHe <annatar.he+github@gmail.com>
2026-04-10 10:37:07 +0800

    fix(app): update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'drizzle-kit'
 
+const dbUrl = process.env.DATABASE_URL!
+
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: dbUrl,
   },
 })
+
